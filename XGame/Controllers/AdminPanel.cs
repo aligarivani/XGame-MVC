0a58768d95ba2bc2346ca21ba3d9924a5624767c
Ali_Garivani <Ali_Garivani@DESKTOP-AMB23MJ>
2025-02-13 21:43:05 +0100

    fix

---
+++ XGame/Controllers/AdminPanel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XGame.Controllers
+{
+    [Authorize ( Roles = "Admin" )]
+    public class AdminPanel : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> AddCategory ( )
+        {
+            ViewBag.ShowFooter = false;
+            return View ( "AddCategory" );
+        }
+    }
+}
