81317633d6b6daa773407c381958f6db76700a65
Ali_Garivani <Ali_Garivani@DESKTOP-AMB23MJ>
2025-02-14 12:27:36 +0100

    Chenge Ui Category and Fix Name

--- XGame/Controllers/AdminPanel.cs
+++ XGame/Controllers/AdminPanel.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using XGame.Entity;
 
 namespace XGame.Controllers
 {
     [Authorize ( Roles = "Admin" )]
     public class AdminPanel : Controller
     {
-        [HttpGet]
-        public async Task<IActionResult> AddCategory ( )
+        private readonly DataContext.DataContext _dataContext;
+
+        public AdminPanel (DataContext.DataContext _dataContext)
         {
+            this._dataContext = _dataContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ShowCategory ( )
+        {
+            List<CategoryEntity> categories = await _dataContext.Categories.ToListAsync ();
+            ViewBag.cate = categories;
             ViewBag.ShowFooter = false;
-            return View ( "AddCategory" );
+            return View ( "ShowCategory", ViewBag.cate );
         }
     }
 }
---
+++ XGame/Models/CategoryDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace XGame.Models
+{
+    [Index ( nameof ( Title ), IsUnique = true )]
+    public class CategoryDTO
+    {
+        [Required ( ErrorMessage = "Required" )]
+        [MinLength ( 5, ErrorMessage = "min 5 character" )]
+        public string? Title { get; set; }
+
+        [Required ( ErrorMessage = "Required" )]
+        [MinLength ( 5, ErrorMessage = "min 5 character" )]
+        public string? TitleFarsi { get; set; }
+        public string? Description { get; set; }
+    }
+}
