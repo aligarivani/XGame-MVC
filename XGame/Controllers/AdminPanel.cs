984ea56f281317e2dd0659f35419c2990abadfe6
Ali_Garivani <Ali_Garivani@DESKTOP-AMB23MJ>
2025-02-14 13:34:47 +0100

    PopUp Add Category

--- XGame/Controllers/AdminPanel.cs
+++ XGame/Controllers/AdminPanel.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using XGame.Entity;
+using XGame.Models;
 
 namespace XGame.Controllers
 {
@@ -21,6 +22,30 @@ namespace XGame.Controllers
             ViewBag.cate = categories;
             ViewBag.ShowFooter = false;
             return View ( "ShowCategory", ViewBag.cate );
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ShowCategory (CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoryEntity data = new CategoryEntity ();
+                data.Title = category.Title;
+                data.TitleFarsi = category.TitleFarsi;
+                data.Description = category.Description;
+                try
+                {
+                    await _dataContext.Categories.AddAsync ( data );
+                    await _dataContext.SaveChangesAsync ();
+
+                }
+                catch (DbUpdateException ex)
+                {
+                    ModelState.AddModelError ( "", "Erorr Update DB" );
+                    return View ( "ShowCategory" );
+                }
+            }
+            return View ( "ShowCategory" );
         }
     }
 }
